d9c712a6cb6818c6fc0bf95e107d915d2dcf597b
Shamimzz <shamimislam5091@gmail.com>
2022-08-11 19:04:52 +0600

    adde

--- Frontend/src/common/Cart/Cart.jsx
+++ Frontend/src/common/Cart/Cart.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Home from "../../assets/images/icons/Home.svg";
 
 export const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <div className="flex items-center container py-5">
+        <span>
+          <img className="w-5 ml-3" src={Home} alt="" srcset="" />
+        </span>
+        <span>
+          <i class="ml-3 font-md fas fa-light fa-angle-right text-primary"></i>
+        </span>
+        <span className="ml-3">Shopping Cart</span>
+      </div>
+    </div>
+  );
 };
